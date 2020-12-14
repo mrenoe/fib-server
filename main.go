adab79c73aa9fffa6b499e5663917b19b7bc2365
Mike Renoe <mrenoe@gmail.com>
2020-12-13 23:01:53 -0500

    Improved syntax and caching

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var mu sync.Mutex
-var past = make(map[int]string)
+var past = make(map[int]*big.Int, 0)
 var count = 0
 
 func main() {
@@ -18,9 +18,12 @@ func main() {
 	http.HandleFunc("/current", current)
 	http.HandleFunc("/next", next)
 	http.HandleFunc("/previous", previous)
-	port := os.Getenv("PORT")
-	//port := "8080"
-	log.Println("listen on localhost:", port)
+	port, ok := os.LookupEnv("PORT")
+	if ok == false || port == "" {
+		port = "8080"
+	}
+
+	log.Println("listen on localhost:" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
@@ -55,34 +58,37 @@ func solveFib(n int) string {
 	if val, ok := past[n]; ok {
 		log.Println("Using cache!")
 		log.Println(val) // 100-digit Fibonacci number
-		return val
+		return val.String()
 	}
+	switch n {
+	case 0:
+		past[n] = big.NewInt(0)
+		return "0"
+	case 1:
+		past[n] = big.NewInt(1)
+		return "1"
+	default:
+		n1 := past[n-1]
+		n2 := past[n-2]
+		if n1 != nil && n2 != nil {
+			a := new(big.Int)
 
-	n1 := past[n-1]
-	n2 := past[n-2]
-	if n1 != "" && n2 != "" {
-		a := new(big.Int)
-		b := new(big.Int)
-		a, _ = a.SetString(n1, 10)
-		b, _ = b.SetString(n2, 10)
-		a.Add(a, b)
-		log.Println("Using additive cache!")
-		past[n] = a.String()
+			a.Add(n1, n2)
+			log.Println("Using additive cache!")
+			past[n] = a
+			return a.String()
+		}
+		a := big.NewInt(0)
+		b := big.NewInt(1)
+
+		for i := 0; i < n; i++ {
+			// Compute the next Fibonacci number, storing it in a.
+			a.Add(a, b)
+			// Swap a and b so that b is the next number in the sequence.
+			a, b = b, a
+		}
+		log.Println(a) // 100-digit Fibonacci number
+		past[n] = a
 		return a.String()
 	}
-
-	// Initialize two big ints with the first two numbers in the sequence.
-	a := big.NewInt(0)
-	b := big.NewInt(1)
-
-	// Loop while a is smaller than 1e100.
-	for i := 0; i < n; i++ {
-		// Compute the next Fibonacci number, storing it in a.
-		a.Add(a, b)
-		// Swap a and b so that b is the next number in the sequence.
-		a, b = b, a
-	}
-	log.Println(a) // 100-digit Fibonacci number
-	past[n] = a.String()
-	return a.String()
 }
