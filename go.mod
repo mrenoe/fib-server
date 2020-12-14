d9dfbaaf49f956e69ddb256b4d279ca96b88f8ba
Mike Renoe <mrenoe@gmail.com>
2020-12-14 02:21:49 -0500

    Rebuild with fastHTTP

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/mrenoe/fib-server
 
 go 1.13
+
+require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/klauspost/compress v1.11.3 // indirect
+	github.com/valyala/fasthttp v1.18.0
+)
--- main.go
+++ main.go
@@ -1,70 +1,88 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/valyala/fasthttp"
 	"log"
 	"math/big"
-	"net/http"
-	"os"
 	"sync"
 )
 
+var (
+	addr     = flag.String("addr", ":8080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
 var mu sync.Mutex
-var past = make(map[int]*big.Int, 0)
-var count = 0
+var past = make(map[uint]*big.Int, 0)
+var count uint = 0
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/current", current)
-	http.HandleFunc("/next", next)
-	http.HandleFunc("/previous", previous)
-	http.HandleFunc("/reset", reset)
-	port, ok := os.LookupEnv("PORT")
-	if ok == false || port == "" {
-		port = "8080"
+	flag.Parse()
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
 	}
 
-	//log.Println("listen on localhost:" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is not a valid endpoint.\nValid endpoints are /current, /next, /previous.")
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Path()) {
+	case "/current":
+		current(ctx)
+	case "/next":
+		next(ctx)
+	case "/previous":
+		previous(ctx)
+	case "/reset":
+		reset(ctx)
+	default:
+		ctx.Error("Unsupported path\nSupported Paths are /current, /next, and /previous", fasthttp.StatusUnprocessableEntity)
+	}
+
 }
 
-func current(w http.ResponseWriter, r *http.Request) {
+func current(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("text/html")
 	mu.Lock()
 	current := solveFib(count)
-	fmt.Fprintf(w, "%s\n", current)
+	fmt.Fprintf(ctx, "%s\n", current)
 	mu.Unlock()
 }
 
-func reset(w http.ResponseWriter, r *http.Request) {
+func reset(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("text/html")
 	mu.Lock()
 	count = 0
-	fmt.Fprintf(w, "Reset back to 0\n")
+	fmt.Fprintf(ctx, "Reset back to 0\n")
 	mu.Unlock()
 }
 
-func next(w http.ResponseWriter, r *http.Request) {
+func next(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("text/html")
 	mu.Lock()
 	count++
 	current := solveFib(count)
-	fmt.Fprintf(w, "fib(%d) -> %s\n", count, current)
+	fmt.Fprintf(ctx, "fib(%d) -> %s\n", count, current)
 	mu.Unlock()
 }
 
-func previous(w http.ResponseWriter, r *http.Request) {
+func previous(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("text/html")
 	mu.Lock()
 	previous := solveFib(count - 1)
-	fmt.Fprintf(w, "%s\n", previous)
+	fmt.Fprintf(ctx, "%s\n", previous)
 	mu.Unlock()
 }
 
-func solveFib(n int) string {
+func solveFib(n uint) string {
 	if val, ok := past[n]; ok {
 		//log.Println("Using cache!")
-		//log.Println(val) // 100-digit Fibonacci number
+		log.Println(val) //  Fibonacci number
 		return val.String()
 	}
 	switch n {
@@ -87,14 +105,14 @@ func solveFib(n int) string {
 		}
 		a := big.NewInt(0)
 		b := big.NewInt(1)
-
-		for i := 0; i < n; i++ {
+		var i uint
+		for i = 0; i < n; i++ {
 			// Compute the next Fibonacci number, storing it in a.
 			a.Add(a, b)
 			// Swap a and b so that b is the next number in the sequence.
 			a, b = b, a
 		}
-		//log.Println(a) // 100-digit Fibonacci number
+		log.Println(a) // 100-digit Fibonacci number
 		past[n] = a
 		return a.String()
 	}
